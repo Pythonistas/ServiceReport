1203c8f39afc632259aa06fe8275bb62f227f8f7
Chris Phillips <crphillip@hotmail.com>
2015-06-26 23:14:10 -0700

    Stubbing out initial methods; adding 'business_unit_for_user' implementation

--- ServiceReport.py
+++ ServiceReport.py
@@ -1,10 +1,21 @@
-import data
+﻿import data
 
 def print_users():
     for name, business_unit in data.users:
         print ("Name: '{0}' Business Unit: '{1}'".format(name, business_unit))
         
 def business_unit_for_user(user_name):
-    # Your code goes here!
-    pass # remove this line once you are done!
+    for name, business_unit in data.users:
+        if user_name == name:
+            return business_unit
 
+#return [(business unit, service), (business unit, service), ...]
+def read_input(input_file): #[CRP]
+    pass
+
+#return {service, {business unit, count}, {business unit, count},...} [LR]
+def services_by_business_unit(): #[LR]
+    pass
+
+def write_report(output_file): #[CRP]
+    pass
